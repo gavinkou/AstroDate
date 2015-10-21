a721721137afc5cca2041cb97de8059056277b96
Ashley Marando <a.marando@me.com>
2015-10-21 02:39:49 -0400

    documentation

--- src/Marando/AstroDate/TimeStandard.php
+++ src/Marando/AstroDate/TimeStandard.php
@@ -2,30 +2,72 @@
 
 namespace Marando\AstroDate;
 
-class TimeStd {
+/**
+ * Represents an astronomical time standard such as TT or TDB
+ */
+class TimeStandard {
 
-  protected $value;
+  //----------------------------------------------------------------------------
+  // Constructors
+  //----------------------------------------------------------------------------
 
   public function __construct($value) {
     $this->value = $value;
   }
 
+  // // // Static
+
+  /**
+   * Coordinated Universal Time (UTC)
+   * @return static
+   */
   public static function UTC() {
     return new static('UTC');
   }
 
+  /**
+   * International Atomic Time (TAI)
+   * @return static
+   */
   public static function TAI() {
     return new static('TAI');
   }
 
+  /**
+   * Terrestrial Dynamic Time (TT or TDT)
+   * @return static
+   */
   public static function TT() {
     return new static('TT');
   }
 
+  /**
+   * Barycentric Dynamic Time (TDB)
+   * @return static
+   */
   public static function TDB() {
     return new static('TDB');
   }
 
+  //----------------------------------------------------------------------------
+  // Properties
+  //----------------------------------------------------------------------------
+
+  /**
+   * Value of this instance
+   * @var string
+   */
+  protected $value;
+
+  //----------------------------------------------------------------------------
+  // Functions
+  //----------------------------------------------------------------------------
+  // // // Overrides
+
+  /**
+   * Represents this instance as a string
+   * @return string
+   */
   public function __toString() {
     return (string)$this->value;
   }
